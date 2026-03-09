c1a46367047478372ca821fd10dd601e2cba4231
Christna Yosua Rotinsulu <yosuamic123@gmail.com>
2026-03-09 21:31:06 +0700

    [RED] Add Order model skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Builder
+@Getter
+public class Order {
+
+    String id;
+    List<Product> products;
+    Long orderTime;
+    String author;
+
+    @Setter
+    String status;
+
+    public Order(String id, List<Product> products, Long orderTime, String author) {
+        this.id = id;
+        this.products = products;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
+    }
+
+    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        this.id = id;
+        this.products = products;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = status;
+    }
+}
