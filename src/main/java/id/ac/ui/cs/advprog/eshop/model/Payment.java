d3debba83c88918252b0c617eb4b396c014aa45a
Christna Yosua Rotinsulu <yosuamic123@gmail.com>
2026-03-10 14:04:24 +0700

    [REFACTOR] Improve Payment model structure and fix logic in constructor for PaymentStatus

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -8,17 +8,18 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Payment {
+
     private String id;
     private String orderId;
     private String method;
     private String status;
     private Map<String, String> paymentData;
 
-    public Payment(String id, String orderId, String method, String status, Map<String, String> paymentData) {
+    public Payment(String id, String orderId, String method, Map<String, String> paymentData) {
         this.id = id;
         this.orderId = orderId;
         this.method = method;
-        this.status = status;
         this.paymentData = paymentData;
+        this.status = "PENDING";
     }
 }
